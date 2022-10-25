aa2ffb00f9401e3cad7f454b58cdaff3d5dcecb1
Nadia <wzmjohn2018@163.com>
2022-10-25 19:10:15 +0800

    simplification

--- 221024/name-correct.py
+++ 221024/name-correct.py
@@ -2,9 +2,7 @@ import os
 import re
 
 picPath = r'O:\任务\手势'
-# pPath = r'O:\任务\手势\code'
 matchPattern = r'IMG_(.*).MP4'
-# matchPattern = r'p-IMG_4155.MP4'
 rel_file_num = 4178
 file_no = 55
 
@@ -15,10 +13,7 @@ file_num_incre = 0
 
 for i in range(0, len(filelist)) : 
     pic_name = filelist[i]
-    # print(pic_name)
-    #字符串操作，取出IMG_后面的数字
     matchObj = re.match(matchPattern, pic_name)
-    # print(matchObj)
     if matchObj:
         cur_file_num = matchObj.group(1)
         if( int(cur_file_num) - loop_pointer == 1):
@@ -31,7 +26,6 @@ for i in range(0, len(filelist)) :
                 cor_file_name = 'p' + str(cor_file_num) + '-' + pic_name
             print("before correct:",pic_name," -> after correct:",cor_file_name)
             os.rename(picPath + "\\" + pic_name, cor_file_name)
-            # print(picPath + "\\" + pic_name)
         else:
             loop_pointer = int(cur_file_num)
             print(cur_file_num, 'not continous:', pic_name)
